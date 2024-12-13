d1b7231c3b400fc45b81896c159fbdfde98d04ce
sarthakbansal90 <bansalsarthak9000@gmail.com>
2024-12-13 13:54:03 +0530

    my msg

---
+++ src/Mycontact.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+function Mycontact() {
+  return (
+    <Fragment>
+        <div className='first'></div>
+      <h2>this is heading</h2>
+      <p>lorem lorem lorem</p>
+    </Fragment>
+  )
+}
+
+export default Mycontact
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Mycontact from './Mycontact';
+import "./style.css";
+import Xys, { About, About2} from './Myabout';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h2>sdjfnlfmnsl</h2>
+    <div>dffjewofjsd</div>
+    <About></About>
+    <About2></About2>
+    <Mycontact />
+   
+  </React.StrictMode>
+);
+
+
